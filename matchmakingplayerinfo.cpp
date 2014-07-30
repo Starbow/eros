de8deb8e32815b9fc6d2bbaf3883ae6ed3d56dc9
Veritasimo <veritasimo@gmail.com>
2014-07-30 11:38:43 +0100

    Remove MMR. Add division rank.

--- matchmakingplayerinfo.cpp
+++ matchmakingplayerinfo.cpp
@@ -5,15 +5,13 @@ MatchmakingPlayerInfo::MatchmakingPlayerInfo(const QString &username, const Divi
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-
-
 	
 	if (stats->placements() > 0) {
 		ui.lblDivision->setText(tr("%n placement(s) remaining", "", stats->placements()));
 		ui.lblPoints->setText(tr("%n point(s)", "", stats->points()));
 	} else {
-		ui.lblDivision->setText("");
-		ui.lblPoints->setText(QString("%1\n%2").arg(tr("%n point(s)", "", stats->points()), tr("%n MMR", "", stats->rating())));
+        ui.lblDivision->setText(tr("Rank %n", "", stats->divisionRank()));
+        ui.lblPoints->setText(tr("%n point(s)", "", stats->points()));
 	}
 	
 	ui.lblLocalPlayerLeagueImage->setText(division->name());
